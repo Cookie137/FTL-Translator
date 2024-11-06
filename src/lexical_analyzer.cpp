a1c8f715da4af6127fffddf7fd28bea2b215645f
Cookie137 <k.egor2007@gmail.com>
2024-11-06 09:16:04 +0300

    Temp commit to create DKA branch

--- src/lexical_analyzer.cpp
+++ src/lexical_analyzer.cpp
@@ -5,10 +5,10 @@ LexicalAnalyzer::LexicalAnalyzer(std::string keywords_file) {
     if (!file.is_open()) {
         throw std::runtime_error("Failed to open keywords file: " + keywords_file);
     }
-    std::string word;
+    std::string keyword;
     keywords_trie_ = new Trie();
-    while (file >> word) {
-        keywords_trie_->AddString(word);
+    while (file >> keyword) {
+        keywords_trie_->AddString(keyword);
     }
     file.close();
 }
@@ -18,7 +18,7 @@ LexicalAnalyzer::~LexicalAnalyzer() {
     delete program_;
 }
 
-std::vector<Token> LexicalAnalyzer::Tokenize(std::string program_file) {
+std::vector<Token*> LexicalAnalyzer::Tokenize(std::string program_file) {
     try {
         std::ifstream file("../config/" + program_file, std::ifstream::binary);
         file.seekg(0, file.end);
@@ -30,13 +30,14 @@ std::vector<Token> LexicalAnalyzer::Tokenize(std::string program_file) {
     } catch (const std::exception& e) {
         throw std::runtime_error("Failed to open program file: " + program_file);
     }
+    iter_ = 0;
     line_ = column_ = 1;
     cursor_ = program_;
-    token_.clear();
-    parsed_tokens_.clear();
+    word_.clear();
+    tokens_.clear();
     GetChar();
     H();
-    return parsed_tokens_;
+    return tokens_;
 }
 
 bool LexicalAnalyzer::IsAlpha(char c) {
@@ -50,25 +51,63 @@ bool LexicalAnalyzer::IsDigit(char c) {
 void LexicalAnalyzer::GetChar() {
     symbol_ = *cursor_;
     ++cursor_;
+    ++iter_;
+}
+
+void LexicalAnalyzer::PushChar() {
+    word_.push_back(symbol_);
 }
 
 void LexicalAnalyzer::H() {
+    //if (iter_ == )
     if (IsAlpha(symbol_)) {
-        token_.push_back(symbol_);
+        PushChar();
         GetChar();
         ID();
     } else if (IsDigit(symbol_)) {
-        token_.push_back(symbol_);
+        PushChar();
         GetChar();
         NUM();
     } else if (symbol_ == '"') {
         GetChar();
         STR();
+    } else if (symbol_ == '=' || symbol_ == '*' || symbol_ == '/' || symbol_ == '%' ||
+               symbol_ == '!' || symbol_ == '&' || symbol_ == '^' || symbol_ == '|' ||
+               symbol_ == '+' || symbol_ == '-') {
+        PushChar();
+        GetChar();
+        OPR();
+    } else if (symbol_ == ';' || symbol_ == ',' || symbol_ == ':' || symbol_ == '.' ||
+               symbol_ == '(' || symbol_ == ')' || symbol_ == '[' || symbol_ == ']' ||
+               symbol_ == '{' || symbol_ == '}' || symbol_ == '<' || symbol_ == '>') {
+        PushChar();
+        GetChar();
+        PUN();
+    } else if (symbol_ == '\n') {
+
+    } else {
+        GetChar();
+        H();
     }
 }
 
 void LexicalAnalyzer::ID() {
-
+    if (IsAlpha(symbol_) || IsDigit(symbol_)) {
+        PushChar();
+        GetChar();
+        ID();
+    } else {
+        Token* token = new Token(TokenType::IDENTIFIER, word_, line_, column_);
+//        if (keywords_trie_->Contains()) {
+//
+//        } else {
+//
+//        }
+        // if trie ...
+        tokens_.push_back(token);
+        word_.clear();
+        H();
+    }
 }
 
 void LexicalAnalyzer::NUM() {
